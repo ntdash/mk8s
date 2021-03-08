9c6484e6d80930b94c301d806af824a9e2cb7fff
Komla Matthäus EHON <ntdash@outlook.com>
2021-03-08 22:41:25 +0000

    Change Main page Title

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ docker build -t ntdash/m-server:latest -t ntdash/m-server:$SHA ./server
 docker build -t ntdash/m-worker:latest -t ntdash/m-worker:$SHA ./worker
 
 # push builed images 
-   # latest for new applyer
-   # $GIT_SHA based for usaly dev
+# latest for new applyer
+# $GIT_SHA based for usaly dev
 
 docker push ntdash/m-client:latest
 docker push ntdash/m-server:latest
@@ -18,11 +18,9 @@ docker push ntdash/m-worker:$SHA
 
 # kubernetes step
 
-alias k=kubectl
-
 kubectl apply -f k8s
 
 # overite the latest version with a based $GIT_SHA version
-k set image deployments/client-deployment client=ntdash/m-client:$SHA
-k set image deployments/server-deployment server=ntdash/m-server:$SHA
-k set image deployments/worker-deployment worker=ntdash/m-worker:$SHA
+kubectl set image deployments/client-deployment client=ntdash/m-client:$SHA
+kubectl set image deployments/server-deployment server=ntdash/m-server:$SHA
+kubectl set image deployments/worker-deployment worker=ntdash/m-worker:$SHA
